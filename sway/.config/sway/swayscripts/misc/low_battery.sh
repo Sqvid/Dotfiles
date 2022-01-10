4fbeacf5d871993b3ba8ff8b0a7a5bbf19f21f2f
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2022-01-10 17:34:13 +0100

    Save and use DBUS_SESSION_RUN_ADDRESS.

--- sway/.config/sway/swayscripts/misc/low_battery.sh
+++ sway/.config/sway/swayscripts/misc/low_battery.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# DBUS_SESSION_BUS_ADDRESS is needed to show notifications from cron.
+. "${HOME}/.dbus/sway_session"
+export DBUS_SESSION_BUS_ADDRESS
+
 state=$(acpi -b | cut -d ' ' -f 3)
 dietime=$(acpi -b | cut -d ' ' -f 5)
 critical="00:20:00"
 
-if [[ $state = "Discharging," && $dietime < $critical ]]; then
+if [[ ${state} = "Discharging," && ${dietime} < ${critical} ]]; then
 	notify-send "Low battery! Please charge now."
 fi
 
