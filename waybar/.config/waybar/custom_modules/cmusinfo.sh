4a677ac750933868ea4300ef9c720921fee46a07
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2022-01-26 00:40:52 +0100

    Fix wrkspc sort. Simplify cmus. Remove animation.

--- waybar/.config/waybar/custom_modules/cmusinfo.sh
+++ waybar/.config/waybar/custom_modules/cmusinfo.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-while true
-do
-	if [ -z "$(pgrep cmus)" ]; then
-		echo ""
-		sleep 15
-		continue
-	fi
-
-	song=$(cmus-remote -C status | grep "tag title" | cut -d ' ' -f3-)
-	artist=$(cmus-remote -C status | grep "\<tag artist\>" | cut -d ' ' -f3-)
-
-	echo "${song} - ${artist}"
-	sleep 1
-done
+cmus-remote -C 'format_print "%{title} - %{artist}"'
