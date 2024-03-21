9bf93e4b63d77d911fa20aca4e45fc22032f0872
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2024-03-21 12:48:56 +0100

    add comment, adjust style

--- neovim/.config/nvim/lua/lazy-setup.lua
+++ neovim/.config/nvim/lua/lazy-setup.lua
@@ -15,9 +15,10 @@ vim.opt.runtimepath:prepend(lazypath)
 require("lazy").setup({
   lazy = true,
   -- Set up all plugins in the lua/plugins directory.
-  { import = "plugins" },
-  { import = "plugins.lsp" },
-  { import = "plugins.ui" }
+  {import = "plugins"},
+  {import = "plugins.lsp"},
+  {import = "plugins.ui"}
 })
 
+-- Set up LSP related plugins.
 require("lsp-setup")
