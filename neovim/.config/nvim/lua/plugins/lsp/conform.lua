991f9e266e01a5ae8496a3b987ef741ceb41bc44
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2024-06-28 22:08:07 +0100

    flesh out conform config

--- neovim/.config/nvim/lua/plugins/lsp/conform.lua
+++ neovim/.config/nvim/lua/plugins/lsp/conform.lua
@@ -1,12 +1,33 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {
-    formatters_by_ft = {
-      go = {"gofumpt"}
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      desc = "Format buffer",
     },
-    format_on_save = {
-      -- These options will be passed to conform.format()
-      async = true
-    }
-  }
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      go = { "gofumpt" },
+      cpp = { "clang-format" }
+    },
+    -- Set up format-on-save
+    format_after_save = { lsp_fallback = false },
+    -- Customize formatters
+    formatters = {
+      ["clang-format"] = {
+        prepend_args = { "--fallback-style=Google" },
+      },
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
 }
