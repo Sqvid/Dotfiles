135d9cf477da0e7e9449d35e5ff1b4d60241732f
Siddhartha Menon <42359201+Sqvid@users.noreply.github.com>
2025-06-14 23:55:13 +0100

    lazy: remove lazy bootstrap

--- neovim/.config/nvim/lua/lazy-setup.lua
+++ neovim/.config/nvim/lua/lazy-setup.lua
@@ -1,15 +1,5 @@
 -- Bootstrap lazy.nvim if it's not installed.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath
-  }
-end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
